a67e8a76d8bed9dcc9a67cef55460684ced39405
Rob Ramsay <robert.ramsay@canterbury.ac.nz>
2010-05-16 15:53:44 +1200

    Added coding notes re: running imap-filter.

--- run-imapfilter.timecap.sh
+++ run-imapfilter.timecap.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+#
+#	Coding Notes:
+#
+#		- for the production environment also consider having 
+#		  a script checking that the server is working, and have 
+#		  it contact you when there is an error. ie check that 
+#		  imapfilter is still looping correctly, and that perl 
+#		  scripts are giving reasonable output.
+#
 
 while (true)
 do
